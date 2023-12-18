84f2237bfc140a72782e7680c12749f54b27e9ba
PgmJun <chltmdwns96@gmail.com>
2023-12-19 03:10:55 +0900

    project init

---
+++ src/main/kotlin/com/th/plu/PluApplication.kt
@@ -0,0 +1,11 @@
+package com.th.plu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PluApplication
+
+fun main(args: Array<String>) {
+	runApplication<PluApplication>(*args)
+}
