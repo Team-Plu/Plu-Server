18340f866c9161df26d6f23591083043e0a98515
PgmJun <chltmdwns96@gmail.com>
2023-12-26 15:08:16 +0900

    ⚡️: fix 잘못된 Entity 클래스 import 수정 #3

--- src/main/kotlin/com/th/plu/domain/member/repository/MemberRepository.kt
+++ src/main/kotlin/com/th/plu/domain/member/repository/MemberRepository.kt
@@ -1,7 +1,7 @@
 package com.th.plu.domain.member.repository
 
+import com.th.plu.domain.member.Member
 import org.springframework.data.jpa.repository.JpaRepository
-import java.lang.reflect.Member
 
 interface MemberRepository: JpaRepository<Member, Long>, MemberRepositoryCustom {
 }
