d1614b74b8e3ec9faaa2f50269f1cd25d07d317f
PgmJun <chltmdwns96@gmail.com>
2024-01-05 21:59:44 +0900

    ✨: feat 알림서버 생성 #9

---
+++ plu-notification/src/main/kotlin/com/th/plu/notification/PluNotificationApplication.kt
@@ -0,0 +1,11 @@
+package com.th.plu.notification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PluNotificationApplication
+
+fun main(args: Array<String>) {
+    runApplication<PluNotificationApplication>(*args)
+}
