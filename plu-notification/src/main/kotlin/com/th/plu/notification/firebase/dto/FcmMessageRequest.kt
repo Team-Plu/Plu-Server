1c43a4ee9606e07ceae33085947c5d0601a9370d
PgmJun <chltmdwns96@gmail.com>
2024-01-16 02:06:43 +0900

    ⚡️: fix Android, Apple 메시지 중복 설정값 제거 #9

--- plu-notification/src/main/kotlin/com/th/plu/notification/firebase/dto/FcmMessageRequest.kt
+++ plu-notification/src/main/kotlin/com/th/plu/notification/firebase/dto/FcmMessageRequest.kt
@@ -4,19 +4,13 @@ class FcmMessageRequest(val validateOnly: Boolean, val message: Message) {
 
     class Message(val token: String, val notification: Notification, val android: Android, val apns: Apns)
     class Notification(val title: String, val body: String)
-    class Data(val title: String, val body: String)
-    class Android(val data: Data)
-    class Apns(val payload: Payload)
-    class Payload(val aps: Aps)
-    class Aps(val alert: Alert)
-    class Alert(val title: String, val body: String)
+    class Android()
+    class Apns()
 
     companion object {
         fun of(fcmToken: String, title: String, body: String): FcmMessageRequest {
             val notification = Notification(title, body)
-            val android = Android(Data(title, body))
-            val apns = Apns(Payload(Aps(Alert(title, body))))
-            val message = Message(fcmToken, notification, android, apns)
+            val message = Message(fcmToken, notification, Android(), Apns())
 
             return FcmMessageRequest(false, message)
         }
