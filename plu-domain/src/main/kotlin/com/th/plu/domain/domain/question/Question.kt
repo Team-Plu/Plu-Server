a76f8ede95cd147220b3fb27d758937a5c863a74
머환임 <udsward@gmail.com>
2024-03-03 14:31:07 +0900

    entity

--- plu-domain/src/main/kotlin/com/th/plu/domain/domain/question/Question.kt
+++ plu-domain/src/main/kotlin/com/th/plu/domain/domain/question/Question.kt
@@ -13,22 +13,32 @@ class Question(
     @Column(name = "question_id")
     var _id: Long? = null,
 
-    @Column(name = "element_type", nullable = false, length = 30)
-    @Enumerated(EnumType.STRING)
-    var elementType: ElementType,
-
-    @Column(name = "question_title", nullable = false, length = 100)
-    var title: String,
-
-    @Column(name = "question_content", nullable = false, length = 300)
-    var content: String,
-
     @OneToMany(mappedBy = "question", fetch = FetchType.LAZY, cascade = [CascadeType.ALL], orphanRemoval = true)
     var answers: List<Answer>,
 
-    @Column(name = "exposed_at", nullable = false, unique = true)
-    val exposedAt: LocalDateTime,
+    elementType: ElementType,
+    title: String,
+    content: String,
+    exposedAt: LocalDateTime,
 ) : BaseEntity() {
     val id: Long
         get() = _id ?: throw PersistenceException("아직 save 되지 않은 entity 의 id 에대한 접근입니다.")
+
+    @Column(name = "element_type", nullable = false, length = 30)
+    @Enumerated(EnumType.STRING)
+    var elementType: ElementType = elementType
+        private set
+
+
+    @Column(name = "question_title", nullable = false, length = 100)
+    var title: String = title
+        private set
+
+    @Column(name = "question_content", nullable = false, length = 300)
+    var content: String = content
+        private set
+
+    @Column(name = "exposed_at", nullable = false, unique = true)
+    var exposedAt: LocalDateTime = exposedAt
+        private set
 }
