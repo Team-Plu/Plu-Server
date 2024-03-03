4b469d773a695a984b269182dd28864c2980eca7
PgmJun <chltmdwns96@gmail.com>
2024-03-04 01:40:03 +0900

    ⚡️: fix @Auth 애노테이션 추가

--- plu-api/src/main/kotlin/com/th/plu/api/controller/answer/AnswerController.kt
+++ plu-api/src/main/kotlin/com/th/plu/api/controller/answer/AnswerController.kt
@@ -1,5 +1,6 @@
 package com.th.plu.api.controller.answer
 
+import com.th.plu.api.config.interceptor.Auth
 import com.th.plu.api.controller.answer.dto.response.AnswerInfoResponse
 import com.th.plu.api.service.answer.AnswerService
 import com.th.plu.common.dto.response.ApiResponse
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController
 class AnswerController(
         private val answerService: AnswerService
 ) {
+    @Auth
     @Operation(summary = "답변 조회")
     @GetMapping("/v1/answer/{answerId}")
     fun findAnswerById(@PathVariable answerId: Long): ApiResponse<AnswerInfoResponse> {
